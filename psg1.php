4fe7ad8fcfb755d3081261688852bf5f9bf707e1
sbeale <Stephen.Beale@stoningtonschools.org>
2016-01-13 07:40:45 -0500

    Create psg1.php

---
+++ psg1.php
@@ -0,0 +1,6 @@
+<div id="full">
+      <h1> Project Safe Grad</h1>
+<h4><center>A Stonington High School Tradition:</center></h4>
+<p></p><center>The Class of 2016 marks 7 years. The class of 2016 encourages all families to view their blog: <a href="http://www.shs2016projectsafegrad.blogspot.com" id="normal">  www.shs2016projectsafegrad.blogspot.com</a>. The blog includes suggestions for underclassman families as well as many details for 2016 families.</center><p></p>
+      
+      <h1>FAQ's</h1>
